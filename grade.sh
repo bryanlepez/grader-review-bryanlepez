82b06e5170ac63b02f5fff094b635cb5b3385350
bryanlepez <130080834+bryanlepez@users.noreply.github.com>
2023-05-25 09:27:59 -0700

    grade.sh progress

--- grade.sh
+++ grade.sh
@@ -8,7 +8,28 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+files=`find ./student-submission`
 
+for file in $files
+do
+    if [[ -f $file ]] && [[ $file == *ListExamples.java* ]]
+    then 
+        echo File Exists
+    fi
+ done
+
+ cp TestListExamples.java grading-area
+ cp GradeServer.java grading-area
+ cp Server.java grading-area
+ cp ListExamples.java
+ cp -r lib grading-area
+
+ cd grading-area
+
+ javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+ java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+ 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
